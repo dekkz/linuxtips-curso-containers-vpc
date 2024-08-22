b7aad9a39010691bdf2e3e457ce91e6387db7d7b
Alex Sandro Barrozo Anastacio <alex_s_anastacio@hotmail.com>
2024-08-22 00:19:46 -0300

    fix config subnets

--- database_subnets.tf
+++ database_subnets.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "database_subnet_1b" {
   vpc_id = aws_vpc.main.id
 
   cidr_block        = "10.0.52.0/24"
-  availability_zone = format("%sa", var.region)
+  availability_zone = format("%sb", var.region)
 
   tags = {
     Name = format("%s-database-subnet-1b", var.project_name)
@@ -28,7 +28,7 @@ resource "aws_subnet" "database_subnet_1c" {
   vpc_id = aws_vpc.main.id
 
   cidr_block        = "10.0.53.0/24"
-  availability_zone = format("%sa", var.region)
+  availability_zone = format("%sc", var.region)
 
   tags = {
     Name = format("%s-database-subnet-1c", var.project_name)
